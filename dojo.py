79fd6f45b7efbf7bfcafc31ef25c4289a471617e
Colton <colton.kinstley@gmail.com>
2018-08-30 18:31:34 +0100

    Can place ships now, they result in ones (will fix that.) Added some tests.

--- dojo.py
+++ dojo.py
@@ -7,40 +7,45 @@ class Board():
 
     def __init__(self):
         self.board = np.zeros(shape=(10,10))
-{}}
-        self.ships = {'carrier': np.ones(shape=(1,5)),
-                'battleship': np.ones(shape=(1,4)),
-                'cruser': np.ones(shape=(1,3)),
-                'submarine': np.ones(shape=(1,3)),
-                'destroyer': np.ones(shape=(1,2))
+
+        self.ships = {'carrier': np.ones(5),
+                'battleship': np.ones(4),
+                'cruser': np.ones(3),
+                'submarine': np.ones(3),
+                'destroyer': np.ones(2)
                 }
 
-        self.letters = {'a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8',
-                        'i':'9','j':'10','k':'11','l':'12','m':'13','n':'14','o':'15','p':'16','q':'17',
-                        'r':'18','s':'19','t':'20','u':'21','v':'22','w':'23','x':'24','y':'25','z':'26'
+        self.letters = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,
+                        'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,
+                        'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26
                        }
 
     def place_ship(self, name, orientation, num, letter):
-        try:
+            num = int(num)
+            lett = self.letters[letter]
+            ship = np.pad(self.ships[name], (num-1, (10-(num-1)-len(self.ships[name]))), 'constant' )
+
             if orientation == 'hor':
-                ship = np.pad(self.ships[name], (num-1, (10-(num-1)-self.ships[name].shape()[1])), 'constant', constant_values=(0, 0) )
-                self.board = self.board[:,self.letters[letter]-1] + ship
+                new_row = self.board[lett-1,:] + ship
+
+                if new_row.flatten()[new_row.argmax()] == 1:
+                    self.board[lett-1,:] = self.board[lett-1,:] + ship
+                else:
+                    raise ValueError('You are trying to place a ship on top of another ship.')
 
             elif orientation == 'ver':
-                # transpose this for vertical ships
-                ship = np.pad(ships[name],(num-1,10-(num-1)-ship[name].shape()[5]))
-                self.board =  self.board[:,self.letters[letter]-1] + ship
+                new_col = self.board[:, lett-1] + ship
+                if new_col.flatten()[new_col.argmax()] == 1:
+                    self.board[:,lett-1] = self.board[:,lett-1] + ship
+                else:
+                    raise ValueError('You are trying to place a ship on top of another ship.')
 
             else:
-                print('Bad orientation.')
-
-        except:
-            pass
-
+                raise ValueError('This paramater must be on of "hor" or "ver"')
 
 
     def hit(self, num, letter):
-        if self.board[num,letters[letter]] == 1:
+        if self.board[num-1,self.letters[letter]-1] > 0:
             return True
         else:
             return False
@@ -52,6 +57,4 @@ class Game(Board):
     def __init__(self, name1, name2):
         self.player1 = {name:name1, points:0}
 
-    def win(self,player):
-        
-
+    # def win(self,player):
---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from dojo import Board
+import numpy as np
+
+class Test_Place_Ship(unittest.TestCase):
+    def test_place_horizontal(self):
+        correct = np.zeros(shape=(10,10))
+        correct[0,:] =  np.array([1,1,1,0,0,0,0,0,0,0])
+        b = Board()
+        b.place_ship('cruser', 'hor', 1, 'a')
+        np.testing.assert_almost_equal(correct, b.board)
+
+    def test_place_vertical(self):
+        correct = np.zeros(shape=(10,10))
+        correct[:,3] =  np.array([0,1,1,1,0,0,0,0,0,0])
+        b = Board()
+        b.place_ship('submarine', 'ver', 2, 'd')
+        np.testing.assert_almost_equal(correct, b.board)
+
+    def test_hit(self):
+        b = Board()
+        b.place_ship('cruser', 'hor', 1, 'a')
+        self.assertTrue(b.hit(1,'a'))
+
+    def test_ship_on_ship(self):
+        with self.assertRaises(ValueError):
+            b = Board()
+            b.place_ship('cruser', 'hor', 1, 'a')
+            b.place_ship('cruser', 'hor', 1, 'a')
+
+if __name__=='__main__':
+    unittest.main()
+
+
