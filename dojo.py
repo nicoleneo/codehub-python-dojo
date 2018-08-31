e1116a25f4050f9d7bf36874949bca1567f29c71
Colton <colton.kinstley@gmail.com>
2018-08-31 15:46:37 +0100

    Places numbered ships. Updated tests to reflect change.

--- dojo.py
+++ dojo.py
@@ -3,49 +3,85 @@
 import numpy as np
 
 
-class Board():
-
+class Board:
     def __init__(self):
-        self.board = np.zeros(shape=(10,10))
+        self.board = np.zeros(shape=(10, 10))
 
-        self.ships = {'carrier': np.ones(5),
-                'battleship': np.ones(4),
-                'cruser': np.ones(3),
-                'submarine': np.ones(3),
-                'destroyer': np.ones(2)
-                }
+        self.ships = {
+            "carrier": ( np.ones(5), 1),
+            "battleship": ( np.ones(4), 2),
+            "cruser": ( np.ones(3), 3),
+            "submarine": ( np.ones(3), 4),
+            "destroyer": ( np.ones(2), 5),
+        }
 
-        self.letters = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,
-                        'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,
-                        'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26
-                       }
+        self.letters = {
+            "a": 1,
+            "b": 2,
+            "c": 3,
+            "d": 4,
+            "e": 5,
+            "f": 6,
+            "g": 7,
+            "h": 8,
+            "i": 9,
+            "j": 10,
+            "k": 11,
+            "l": 12,
+            "m": 13,
+            "n": 14,
+            "o": 15,
+            "p": 16,
+            "q": 17,
+            "r": 18,
+            "s": 19,
+            "t": 20,
+            "u": 21,
+            "v": 22,
+            "w": 23,
+            "x": 24,
+            "y": 25,
+            "z": 26,
+        }
 
     def place_ship(self, name, orientation, num, letter):
-            num = int(num)
-            lett = self.letters[letter]
-            ship = np.pad(self.ships[name], (num-1, (10-(num-1)-len(self.ships[name]))), 'constant' )
+        num = int(num)
+        lett = self.letters[letter]
+        ship_number = self.ships[name][1]
 
-            if orientation == 'hor':
-                new_row = self.board[lett-1,:] + ship
+        if orientation == "hor":
+            ship = np.pad(
+                self.ships[name][0],
+                (lett - 1, (10 - (lett - 1) - len(self.ships[name][0]))),
+                "constant",
+            )
 
-                if new_row.flatten()[new_row.argmax()] == 1:
-                    self.board[lett-1,:] = self.board[lett-1,:] + ship
-                else:
-                    raise ValueError('You are trying to place a ship on top of another ship.')
-
-            elif orientation == 'ver':
-                new_col = self.board[:, lett-1] + ship
-                if new_col.flatten()[new_col.argmax()] == 1:
-                    self.board[:,lett-1] = self.board[:,lett-1] + ship
-                else:
-                    raise ValueError('You are trying to place a ship on top of another ship.')
-
+            if ( self.board.clip(0,1)[num-1, :] + ship ).max() == 1:
+                self.board[num - 1, :] = self.board[num - 1, :] + (ship_number*ship)
             else:
-                raise ValueError('This paramater must be on of "hor" or "ver"')
+                raise ValueError(
+                    "You are trying to place a ship on top of another ship."
+                )
 
+        elif orientation == "ver":
+            ship = np.pad(
+                self.ships[name][0],
+                (num - 1, (10 - (num - 1) - len(self.ships[name][0]))),
+                "constant",
+            )
+
+            if ( self.board.clip(0,1)[:, lett-1] + ship ).max() == 1:
+                self.board[:, lett - 1] = self.board[:, lett - 1] + (ship_number*ship)
+            else:
+                raise ValueError(
+                    "You are trying to place a ship on top of another ship."
+                )
+
+        else:
+            raise ValueError('This paramater must be one of "hor" or "ver"')
 
     def hit(self, num, letter):
-        if self.board[num-1,self.letters[letter]-1] > 0:
+        if self.board[num - 1, self.letters[letter] - 1] > 0:
             return True
         else:
             return False
@@ -53,8 +89,9 @@ class Board():
     def show_board(self):
         print(self.board)
 
+
 class Game(Board):
     def __init__(self, name1, name2):
-        self.player1 = {name:name1, points:0}
+        self.player1 = {name: name1, points: 0}
 
     # def win(self,player):
--- tests.py
+++ tests.py
@@ -5,14 +5,14 @@ import numpy as np
 class Test_Place_Ship(unittest.TestCase):
     def test_place_horizontal(self):
         correct = np.zeros(shape=(10,10))
-        correct[0,:] =  np.array([1,1,1,0,0,0,0,0,0,0])
+        correct[1,:] =  np.array([0,3,3,3,0,0,0,0,0,0])
         b = Board()
-        b.place_ship('cruser', 'hor', 1, 'a')
+        b.place_ship('cruser', 'hor', 2, 'b')
         np.testing.assert_almost_equal(correct, b.board)
 
     def test_place_vertical(self):
         correct = np.zeros(shape=(10,10))
-        correct[:,3] =  np.array([0,1,1,1,0,0,0,0,0,0])
+        correct[:,3] =  np.array([0,4,4,4,0,0,0,0,0,0])
         b = Board()
         b.place_ship('submarine', 'ver', 2, 'd')
         np.testing.assert_almost_equal(correct, b.board)
